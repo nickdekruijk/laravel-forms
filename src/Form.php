3b9a5c8191b836f8608992cce44fe90269a88503
Nick de Kruijk <git@nickdekruijk.nl>
2020-11-18 15:38:03 +0100

    Workking Form::open and close, default attributes

--- src/Form.php
+++ src/Form.php
@@ -4,10 +4,67 @@ namespace NickDeKruijk\LaravelForms;
 
 class Form
 {
-    public function open()
+    /**
+     * The default form attributes, merged with Form::open($attributes)
+     *
+     * @var array
+     */
+    private $attributes = [
+        'method' => 'POST',
+        'action' => null,
+    ];
+
+    /**
+     * Merge default $this->attributes array with new form attributes
+     *
+     * @param array $attributes
+     * @return void
+     */
+    private function merge_attributes(array $attributes)
     {
-        return '<form>' . csrf_token();
+        if (isset($attributes['route'])) {
+            $attributes['action'] = route($attributes['route']);
+            unset($attributes['route']);
+        }
+        $this->attributes = array_merge($this->attributes, $attributes);
     }
+
+    /**
+     * Open a new <form>
+     *
+     * @param array $attributes
+     * @return void
+     */
+    public function open(array $attributes = [])
+    {
+        // Merge attributes with defaults
+        $this->merge_attributes($attributes);
+
+        // Start response
+        $response = '<form';
+
+        // Add all set attributes
+        foreach ($this->attributes as $attribute => $value) {
+            if ($value) {
+                $response .= ' ' . $attribute . '="' . $value . '"';
+            }
+        }
+
+        // End of <form tag
+        $response .= '>';
+
+        // Add hidden CSRF field
+        $response .= csrf_field();
+
+        // Return the complete response
+        return $response;
+    }
+
+    /**
+     * Close a </form>
+     *
+     * @return void
+     */
     public function close()
     {
         return '</form>';
